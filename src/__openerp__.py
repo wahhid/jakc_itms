3398ac78335fa8c106895d6e042c3169a84f83fc
wahhid@gmail.com <wahhid@gmail.com>
2014-05-28 09:51:35 +0700

    Update All

--- src/__openerp__.py
+++ src/__openerp__.py
@@ -6,8 +6,10 @@
     'depends' : ['base_setup','base','hr'],
     'init_xml' : [],
     'data' : [			
+        'security/jakc_itms_security.xml',
+        'security/ir.model.access.csv',	
         'jakc_itms_view.xml',        
-        'hr_department_view.xml',
+        'hr_department_view.xml',        
     ],
     'installable': True,
     'auto_install': False,
